fa44665e7ba608969c426b41cc93caf1019f97fc
Shafayat <shafayathossainkhan@gmail.com>
2017-01-29 16:04:14 +0600

    first commit

--- src/main/java/cars.java
+++ src/main/java/cars.java
@@ -1,5 +1,8 @@
 import org.jsoup.Jsoup;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
@@ -12,18 +15,19 @@ public class cars {
         for (int i = 1; i <=377 ; i++) {
             try {
                 Document pageURL = Jsoup.connect("https://www.carmudi.com.bd/cars/?sort=suggested&page="+i).get();
-                if(i<377)
-                {
-                    for(j=1;j<=30;j++)
-                    {
-
-                    }
-                }
-                else
-                {
-                    for(j=1;j<=16;j++)
-                    {
-                        
+                Elements carURL =pageURL.select(".item-title.type-m");
+                for (Element element :
+                     carURL) {
+                    String link=element.select("a").attr("href");
+                    Document eachCarURL=Jsoup.connect("https://www.carmudi.com.bd"+link).get();
+                    Elements car=eachCarURL.select(".column");
+                    for (Element element1:
+                         car) {
+                        String carModel=element1.select(".title-bar span").text();
+                        String price=element1.select(".type-xl").text();
+                        String color=element1.select("#details .left .block-list li:nth-of-type(1)").text();
+                        String cc=element1.select(".tech-bar.clearfix .column:nth-of-type(4) .type-s").text();
+                        System.out.println(carModel);
                     }
                 }
             } catch (IOException e) {
